1d81c48fa51ec424791ad5698367deb1d203ebc2
Jason Walker <jlwalker@spsmail.org>
2020-06-19 14:12:48 -0500

    Added ModularMsSqliteClient as a helper.

---
+++ Jlw.Standard.Utilities.Data/DbUtility/ModularMsSqliteClient.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Jlw.Standard.Utilities.Data.DbUtility
+{
+    public class ModularMsSqliteClient : ModularDbClient<SqliteConnection, SqliteCommand, SqliteParameter>, IModularDbClient
+    {
+        /*
+        public virtual System.Data.IDbConnection GetConnection(string connString)
+        {
+            return new SqliteConnection(connString);
+        }
+
+        public virtual System.Data.IDbCommand GetCommand(string cmd, System.Data.IDbConnection conn)
+        {
+            return new SqliteCommand(cmd, (SqliteConnection)conn);
+        }
+
+        public virtual System.Data.IDbDataParameter AddParameterWithValue(string paramName, object value, System.Data.IDbCommand cmd)
+        {
+            return ((SqliteCommand) cmd).Parameters.AddWithValue(paramName, value);
+        }
+
+        public virtual System.Data.IDbDataParameter GetNewParameter()
+        {
+            return new SqliteParameter();
+        }
+        */
+    }
+}
