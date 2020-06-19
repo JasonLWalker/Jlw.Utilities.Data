d5ca1bd4294998ea5475a817868e176c4f018c37
Jason Walker <jlwalker@spsmail.org>
2020-06-19 14:14:23 -0500

    Added ModularDataRepositoryBase as a starting point for new data repositories

---
+++ Jlw.Standard.Utilities.Data/DbUtility/ModularDataRepositoryBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+
+namespace Jlw.Standard.Utilities.Data.DbUtility
+{
+    public class ModularDataRepositoryBase
+    {
+        protected string _connString = "";
+        protected IModularDbClient _dbClient;
+
+        public ModularDataRepositoryBase(IModularDbClient dbClient, string connString)
+        {
+            _connString = connString;
+            _dbClient = dbClient;
+        }
+
+        #region Internal Members
+        protected virtual IDbConnection GetConnection(string connString)
+        {
+            if (string.IsNullOrWhiteSpace(connString))
+            {
+                throw new ArgumentException("Invalid Connection String. Connection string may not be empty or null.");
+            }
+
+            IDbConnection dbConn = _dbClient?.GetConnection(connString);
+
+            if (dbConn == null)
+            {
+                throw new InvalidOperationException("Unable to retrieve a valid database connection.");
+            }
+
+            return dbConn;
+        }
+        protected virtual IDbCommand GetCommand(string sqlString, IDbConnection dbConn)
+        {
+            if (string.IsNullOrWhiteSpace(sqlString))
+            {
+                throw new ArgumentException("Invalid command string. SQL command may not be empty or null.");
+            }
+
+            IDbCommand dbCmd = _dbClient?.GetCommand(sqlString, dbConn);
+
+            if (dbCmd == null)
+            {
+                throw new InvalidOperationException("Unable to initialize database command.");
+            }
+
+            return dbCmd;
+        }
+        #endregion
+        
+    }
+}
