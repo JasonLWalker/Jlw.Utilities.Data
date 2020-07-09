40c702917d1ae58cd990b2297e36eb77b299cf3d
Jason Walker <jlwalker@spsmail.org>
2020-07-09 16:39:25 -0500

    Added DbUtility schema interfaces

---
+++ Jlw.Standard.Utilities.Data/DbUtility/Interfaces/IDatabaseSchema.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jlw.Standard.Utilities.Data.DbUtility
+{
+    public interface IDatabaseSchema
+    {
+        string Name { get; }
+        string DefaultCollation { get; }
+        DateTime CreationDate { get; }
+
+    }
+}
---
+++ Jlw.Standard.Utilities.Data/DbUtility/Interfaces/ITableSchema.cs
@@ -0,0 +1,10 @@
+namespace Jlw.Standard.Utilities.Data.DbUtility
+{
+    public interface ITableSchema
+    {
+        string Database { get; }
+        string Schema { get;  }
+        string Name { get; }
+        string Type { get; }
+    }
+}
