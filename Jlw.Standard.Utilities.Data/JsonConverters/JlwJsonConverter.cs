29137d3533071f0959e8d9d5d1267e74fc9af497
Jason Walker <jlwalker@spsmail.org>
2020-07-22 09:02:40 -0500

    Moved package management to GitHub workflow

---
+++ Jlw.Standard.Utilities.Data/JsonConverters/JlwJsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Jlw.Standard.Utilities.Data
+{
+    public class JlwJsonConverter<T> : JsonConverter
+    {
+        public override bool CanWrite { get { return false; } }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return DataUtility.ParseAs(typeof(T), reader.Value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+    }
+}
