74f575e7d4a3ebe64d8de0c6673adbe0095ea00d
Jason Walker <jlwalker@spsmail.org>
2020-07-22 09:58:05 -0500

    Added NullDb connection, command, and parameter constructs

---
+++ Jlw.Standard.Utilities.Data/DbUtility/Models/ModularNullDbCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+
+namespace Jlw.Standard.Utilities.Data.DbUtility
+{
+    class NullDbCommand : NullDbCommand<NullDbParameter>
+    {
+
+    }
+
+    class NullDbCommand<TParam> : IDbCommand 
+        where TParam : IDbDataParameter, new()
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbDataParameter CreateParameter()
+        {
+            return new TParam();
+        }
+
+        public int ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader ExecuteReader()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader ExecuteReader(CommandBehavior behavior)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ExecuteScalar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Prepare()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CommandText { get; set; }
+        public int CommandTimeout { get; set; }
+        public CommandType CommandType { get; set; }
+        public IDbConnection Connection { get; set; }
+        public IDataParameterCollection Parameters { get; }
+        public IDbTransaction Transaction { get; set; }
+        public UpdateRowSource UpdatedRowSource { get; set; }
+    }
+}
---
+++ Jlw.Standard.Utilities.Data/DbUtility/Models/NullDbConnection.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+
+namespace Jlw.Standard.Utilities.Data.DbUtility
+{
+    class NullDbConnection : NullDbConnection<NullDbCommand>
+    {
+
+    }
+
+    class NullDbConnection<TCommand> : IDbConnection
+        where TCommand : IDbCommand, new()
+    {
+    public void Dispose()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public IDbTransaction BeginTransaction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IDbTransaction BeginTransaction(IsolationLevel il)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ChangeDatabase(string databaseName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Close()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IDbCommand CreateCommand()
+    {
+        return new TCommand();
+    }
+
+    public void Open()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string ConnectionString { get; set; }
+    public int ConnectionTimeout { get; }
+    public string Database { get; }
+    public ConnectionState State { get; }
+    }
+}
