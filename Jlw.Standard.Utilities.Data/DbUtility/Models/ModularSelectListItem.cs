707f56cba67af496077bf561cb1b0ff431975792
Jason Walker <jlwalker@spsmail.org>
2020-07-29 11:19:18 -0500

    Fixed reversed properties in ModularSelectList and renamed class and interface

--- Jlw.Standard.Utilities.Data/DbUtility/Models/ModularSelectListItem.cs
+++ Jlw.Standard.Utilities.Data/DbUtility/Models/ModularSelectListItem.cs
@@ -1,48 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace Jlw.Standard.Utilities.Data.DbUtility.Models
+namespace Jlw.Standard.Utilities.Data.DbUtility
 {
-    public interface IModularSelectListItem<TSelectListItem, TInterface, TModel>
+    public interface IModularSelectList<TSelectListItem, TInterface, TModel>
         where TModel : class, TInterface
     {
         IEnumerable<TSelectListItem> Items { get; }
 
         void Refresh();
-    }
-
-    public class ModularSelectListItem<TSelectListItem, TInterface, TModel> : IModularSelectListItem<TSelectListItem, TInterface, TModel>
-        where TModel : class, TInterface
-    {
-        protected readonly List<TSelectListItem> _items = new List<TSelectListItem>();
-        protected readonly IModularDataRepositoryBase<TInterface, TModel> _repo;
-
-        public IEnumerable<TSelectListItem> Items => _items;
-
-        public ModularSelectListItem(IModularDataRepositoryBase<TInterface, TModel> repo)
-        {
-            _repo = repo;
-            Initialize();
-        }
-
-        protected void Initialize()
-        {
-            _items.Clear();
-            IEnumerable<KeyValuePair<string, string>> aList = _repo.GetKvpList();
-            foreach (var o in aList)
-            {
-                TSelectListItem item = (TSelectListItem) Activator.CreateInstance(typeof(TSelectListItem));
-                typeof(TSelectListItem).GetProperty("Text")?.SetMethod?.Invoke(item, new object[] {o.Key});
-                typeof(TSelectListItem).GetProperty("Value")?.SetMethod?.Invoke(item, new object[] {o.Value});
-                    //, new object[] {o.Key, o.Value}
-                _items.Add(item);
-            }
-        }
-
-        public void Refresh()
-        {
-            Initialize();
-        }
     }
 }
