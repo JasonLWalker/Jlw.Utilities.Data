fa09d4b646c2f657ce421cdcce689499c3647021
Jason Walker <jlwalker@spsmail.org>
2020-08-20 01:44:15 -0500

    NullDbConnection Unit Tests

---
+++ Jlw.Standard.Utilities.Data.Tests/UnitTests/DbUtilityModels/NullDbConnectionFixture/BaseNullDbConnectionFixture.cs
@@ -0,0 +1,10 @@
+﻿using Jlw.Standard.Utilities.Data.DbUtility;
+using Jlw.Standard.Utilities.Testing;
+
+namespace Jlw.Standard.Utilities.Data.Tests.UnitTests.DbUtilityModels.NullDbConnectionFixture
+{
+    public class BaseNullDbConnectionFixture : BaseModelFixture<NullDbConnection>
+    {
+
+    }
+}
---
+++ Jlw.Standard.Utilities.Data.Tests/UnitTests/DbUtilityModels/NullDbConnectionFixture/MemberFunctions/NullDbConnection_Dispose.cs
@@ -0,0 +1,22 @@
+﻿using Jlw.Standard.Utilities.Data.DbUtility;
+using Jlw.Standard.Utilities.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jlw.Standard.Utilities.Data.Tests.UnitTests.DbUtilityModels.NullDbConnectionFixture
+{
+    [TestClass]
+    public class NullDbConnection_Dispose : BaseModelFixture<NullDbConnection>
+    {
+        [TestMethod]
+        public void Should_NotThrowException()
+        {
+            using (var sut = new NullDbConnection())
+            {
+                // do nothing;
+            } // Dispose is called here
+
+            Assert.IsTrue(true);
+        }
+
+    }
+}
