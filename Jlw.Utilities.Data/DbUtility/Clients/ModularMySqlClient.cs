32b09553398193c4ade21645ffa43765d519fe38
Jason Walker <jlwalker@spsmail.org>
2020-09-23 13:05:18 -0500

    clean up commented code

--- Jlw.Utilities.Data/DbUtility/Clients/ModularMySqlClient.cs
+++ Jlw.Utilities.Data/DbUtility/Clients/ModularMySqlClient.cs
@@ -93,25 +93,5 @@ namespace Jlw.Utilities.Data.DbUtility
             return oReturn;
         }
 
-        /*
-        public System.Data.IDbConnection GetConnection(string connString)
-        {
-            return new MySql.Data.MySqlClient.MySqlConnection(connString);
-        }
-        public System.Data.IDbCommand GetCommand(string cmd, System.Data.IDbConnection conn)
-        {
-            return new MySql.Data.MySqlClient.MySqlCommand(cmd, (MySql.Data.MySqlClient.MySqlConnection)conn);
-        }
-
-        public System.Data.IDbDataParameter AddParameterWithValue(string paramName, object value, System.Data.IDbCommand cmd)
-        {
-            return ((MySql.Data.MySqlClient.MySqlCommand) cmd).Parameters.AddWithValue(paramName, value);
-        }
-
-        public System.Data.IDbDataParameter GetNewParameter()
-        {
-            return new MySql.Data.MySqlClient.MySqlParameter();
-        }
-        */
     }
 }
