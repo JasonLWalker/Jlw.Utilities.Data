95bcf53829a8e20329a3d5403f91a6d46d9b7f9f
Jason Walker <jlwalker@spsmail.org>
2020-09-25 09:59:35 -0500

    split interface into separate file

---
+++ Jlw.Utilities.Data/DbUtility/Interfaces/IModularDataRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+
+namespace Jlw.Utilities.Data.DbUtility
+{
+    public interface IModularDataRepository<TInterface, TModel> where TModel : TInterface
+    {
+        string ConnectionString { get; }
+        DbConnectionStringBuilder ConnectionBuilder { get; }
+
+        IModularDbClient DbClient { get; }
+
+        TInterface GetRecordObject(TInterface objSearch, string definitionName);
+        TReturn GetRecordObject<TReturn>(TInterface objSearch, string definitionName);
+
+        RepositoryMethodDefinition<TInterface, TModel> AddNewDefinition(string name, string query, IEnumerable<string> paramList, CommandType cmdType = CommandType.Text, RepositoryRecordCallback callback = null);
+        RepositoryMethodDefinition<TInterface, TModel> AddNewDefinition(string name, string query, IEnumerable<KeyValuePair<string, object>> paramList, CommandType cmdType = CommandType.Text, RepositoryRecordCallback callback = null);
+        RepositoryMethodDefinition<TInterface, TModel> AddNewDefinition(string name, string query, IEnumerable<IDbDataParameter> paramList = null, CommandType cmdType = CommandType.Text, RepositoryRecordCallback callback = null);
+    }
+}
