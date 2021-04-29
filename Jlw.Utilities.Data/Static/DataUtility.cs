f5fc3e6cb7367645d5167dc9f295952e19cae074
Jason Walker <jason@draxjinn.com>
2021-04-29 10:48:24 -0500

    Added reflection methods and moved reflection-related methods into separate file

--- Jlw.Utilities.Data/Static/DataUtility.cs
+++ Jlw.Utilities.Data/Static/DataUtility.cs
@@ -142,50 +142,6 @@ namespace Jlw.Utilities.Data
 
 
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static object GetObjectValue(object obj, string key=null)
-        {
-            object o = obj;
-            try
-            {
-                if (!string.IsNullOrWhiteSpace(key))
-                {
-                    switch (obj)
-                    {
-                        case IEnumerable<KeyValuePair<string, object>> enumerable:
-                            return enumerable.FirstOrDefault(kvp => kvp.Key == key).Value;
-                        case IDictionary dict:
-                            return dict.Contains(key) ? dict[key] : null;
-                        case IDataRecord record:
-                            return (Enumerable.Range(0, record.FieldCount).Any(x => record.GetName(x) == key)) ? record[key] : DBNull.Value;
-                    }
-
-                    var t = obj?.GetType();
-
-                    var fieldInfo = t?.GetFields().FirstOrDefault(x => x.Name == key);
-                    if (fieldInfo != null)
-                        return fieldInfo.GetValue(obj);
-
-                    var propInfo = t?.GetProperties().FirstOrDefault(x => x.Name == key && x.CanRead);
-                    if (propInfo != null)
-                        return propInfo.GetValue(obj);
-                }
-
-                if (obj is Enum)
-                {
-                    Type enumType = Enum.GetUnderlyingType(obj.GetType());
-                    return Convert.ChangeType(obj, enumType);
-                }
-            }
-            catch
-            {
-                // Ignore
-                o = null;
-            }
-
-            return o;
-        }
-
         public static string GetCaller([CallerMemberName] string caller = null)
         {
             return caller;
@@ -348,73 +304,5 @@ namespace Jlw.Utilities.Data
         }
 
         
-        public static string GetTypeArgs(Type[] typeArray)
-        {
-            string sArgList = "";
-            foreach (Type typ in typeArray)
-            {
-                string sType = GetTypeName(typ);
-                sArgList += $"{sType}, ";
-            }
-
-
-            return sArgList.Trim(',', ' ');
-        }
-
-        public static string GetTypeName(Type t)
-        {
-            var tc = Type.GetTypeCode(t);
-            switch (tc)
-            {
-                case TypeCode.Boolean:
-                    return "bool";
-                case TypeCode.Byte:
-                    return "byte";
-                case TypeCode.Char:
-                    return "char";
-                case TypeCode.Double:
-                    return "double";
-                case TypeCode.Int16:
-                    return "short";
-                case TypeCode.Int32:
-                    return "int";
-                case TypeCode.Int64:
-                    return "long";
-                case TypeCode.Object:
-                    if (t.IsGenericType)
-                    {
-                        if (t.GetGenericTypeDefinition() == typeof(Nullable<>))
-                            return GetTypeName(t.GetGenericArguments()[0]) + "?";
-                        else
-                        {
-                            return GetGenericTypeString(t) + ", ";
-                        }
-                    }
-
-                    break;
-                case TypeCode.Single:
-                    return "float";
-                case TypeCode.String:
-                    return "string";
-            }
-
-            return t.Name;
-        }
-
-        // from https://stackoverflow.com/questions/2448800/given-a-type-instance-how-to-get-generic-type-name-in-c#2448918
-        public static string GetGenericTypeString(Type t)
-        {
-            if (!t.IsGenericType)
-                return GetTypeName(t);
-            string genericTypeName = t.GetGenericTypeDefinition().Name;
-            genericTypeName = genericTypeName.Substring(0,
-                genericTypeName.IndexOf('`'));
-            string genericArgs = string.Join(", ",
-                t.GetGenericArguments()
-                    .Select(ta => GetGenericTypeString(ta)).ToArray());
-            return genericTypeName + "<" + genericArgs + ">";
-        }
-
-
     }
 }
