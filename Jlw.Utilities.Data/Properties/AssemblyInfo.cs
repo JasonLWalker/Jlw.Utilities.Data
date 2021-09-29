4ea2301999cb673dc63f1f63898efc4415e57f2e
Jason Walker <jason@draxjinn.com>
2021-09-29 16:26:55 -0500

    Refactor build scripts

--- Jlw.Utilities.Data/Properties/AssemblyInfo.cs
+++ Jlw.Utilities.Data/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿/*
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("4.2.*")]
+*/
