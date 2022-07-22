3b6b4e8c394b3a9c8f808e96b886159e3b04e67e
Jason Walker <jason@draxjinn.com>
2022-07-22 10:03:40 -0500

    Added data parsing extension.

---
+++ Jlw.Extensions.DataParsing/ObjectParsingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Jlw.Utilities.Data;
+
+namespace Jlw.Extensions.DataParsing
+{
+    public static partial class ObjectParsingExtensions
+    {
+        public static T ParseTo<T>(this object obj, string key = null)
+        {
+            return DataUtility.Parse<T>(obj, key);
+        }
+
+        public static T GenerateRandom<T>(object minLength, object maxLength = null, string validChars = null, Random rand = null)
+        {
+            return DataUtility.GenerateRandom<T>(minLength, maxLength, validChars, rand);
+        }
+
+    }
+}
