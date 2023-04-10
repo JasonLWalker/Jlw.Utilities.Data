345adeb3a14acd9d365bd7a63ec152a90527a6c8
Jason Walker <jlwalker@spsmail.org>
2023-04-10 16:01:57 -0500

    implement CommandTimeout in options

--- Jlw.Extensions.ModularDbClient/ModularDbClientExtensions.cs
+++ Jlw.Extensions.ModularDbClient/ModularDbClientExtensions.cs
@@ -7,35 +7,35 @@ namespace Jlw.Extensions.ModularDbClient
 {
     public static partial class ModularDbClientExtensions
     {
-        public static IServiceCollection AddModularDbClient<TConnection, TCommand, TParam, TConnBuilder>(this IServiceCollection services, string? connectionString = null, IModularDbClient<TConnection, TCommand, TParam, TConnBuilder>? dbClient = null)
+		public static IServiceCollection AddModularDbClient<TConnection, TCommand, TParam, TConnBuilder>(this IServiceCollection services, string? connectionString = null, IModularDbClient<TConnection, TCommand, TParam, TConnBuilder>? dbClient = null, int commandTimeout = 30)
             where TConnection : IDbConnection, new()
             where TCommand : IDbCommand, new()
             where TParam : IDbDataParameter, new()
             where TConnBuilder : DbConnectionStringBuilder, new()
         {
-            dbClient ??= new ModularDbClient<TConnection, TCommand, TParam, TConnBuilder>();
+            dbClient ??= new ModularDbClient<TConnection, TCommand, TParam, TConnBuilder>() { CommandTimeout = commandTimeout };
             services.AddSingleton<IModularDbClient>(dbClient);
-            if (!string.IsNullOrWhiteSpace(connectionString)) services.AddSingleton<IModularDbOptions>(new ModularDbOptions() { ConnectionString = connectionString, DbClient = dbClient });
+            if (!string.IsNullOrWhiteSpace(connectionString)) services.AddSingleton<IModularDbOptions>(new ModularDbOptions() { ConnectionString = connectionString, DbClient = dbClient, CommandTimeout = dbClient.CommandTimeout});
             return services;
         }
 
-        public static IServiceCollection AddModularDbClient<TConnection, TCommand, TParam>(this IServiceCollection services, string? connectionString = null, IModularDbClient? dbClient = null)
+        public static IServiceCollection AddModularDbClient<TConnection, TCommand, TParam>(this IServiceCollection services, string? connectionString = null, IModularDbClient? dbClient = null, int commandTimeout = 30)
             where TConnection : IDbConnection, new()
             where TCommand : IDbCommand, new()
             where TParam : IDbDataParameter, new()
         {
-            dbClient ??= new ModularDbClient<TConnection, TCommand, TParam>();
+            dbClient ??= new ModularDbClient<TConnection, TCommand, TParam>() { CommandTimeout = commandTimeout };
             services.AddSingleton<IModularDbClient>(dbClient);
-            if (!string.IsNullOrWhiteSpace(connectionString)) services.AddSingleton<IModularDbOptions>(new ModularDbOptions() { ConnectionString = connectionString, DbClient = dbClient });
+            if (!string.IsNullOrWhiteSpace(connectionString)) services.AddSingleton<IModularDbOptions>(new ModularDbOptions() { ConnectionString = connectionString, DbClient = dbClient, CommandTimeout = dbClient.CommandTimeout });
             return services;
         }
 
-        public static IServiceCollection AddModularDbClient<TConnection>(this IServiceCollection services, string? connectionString = null, IModularDbClient? dbClient = null)
+        public static IServiceCollection AddModularDbClient<TConnection>(this IServiceCollection services, string? connectionString = null, IModularDbClient? dbClient = null, int commandTimeout = 30)
             where TConnection : IDbConnection, new()
         {
-            dbClient ??= new ModularDbClient<TConnection>();
+            dbClient ??= new ModularDbClient<TConnection>(){CommandTimeout = commandTimeout};
             services.AddSingleton<IModularDbClient>(dbClient);
-            if (!string.IsNullOrWhiteSpace(connectionString)) services.AddSingleton<IModularDbOptions>(new ModularDbOptions() { ConnectionString = connectionString, DbClient = dbClient });
+            if (!string.IsNullOrWhiteSpace(connectionString)) services.AddSingleton<IModularDbOptions>(new ModularDbOptions() { ConnectionString = connectionString, DbClient = dbClient, CommandTimeout = dbClient.CommandTimeout });
             return services;
         }
 
