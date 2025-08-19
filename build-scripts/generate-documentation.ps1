9efc6d01e49c3c3d72015dfd5b701cc5b897a3f3
Jason Walker <jlwalker@r12mail.sps.org>
2025-08-19 14:02:21 -0500

    Change index.md to README.md

--- build-scripts/generate-documentation.ps1
+++ build-scripts/generate-documentation.ps1
@@ -2,4 +2,4 @@ param([string]$sourcePath, [string]$outputPath)
 
 Write-Host "===================== Generating Documentation Markdown ====================="
 dotnet tool install xmldoc2markdown
-dotnet xmldoc2md $sourcePath\Jlw.Utilities.Data.dll --output $outputPath\Jlw.Utilities.Data --member-accessibility-level public --back-button
+dotnet xmldoc2md $sourcePath\Jlw.Utilities.Data.dll --output $outputPath\Jlw.Utilities.Data --member-accessibility-level public --back-button --index-page-name README
